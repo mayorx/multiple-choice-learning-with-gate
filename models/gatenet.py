dfe34df478be874eeb1b4de2a6080da28b924422
mayorx <chenliangyux@gmail.com>
2018-04-18 13:27:12 +0800

    add a very simple gatenet

---
+++ models/gatenet.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GateNet(nn.Module):
+    def __init__(self, model_nums):
+        super(GateNet, self).__init__()
+        self.fc1 = nn.Linear(3*32*32, 20)
+        self.fc2 = nn.Linear(20, model_nums)
+
+    def forward(self, x):
+        x = x.view(-1, 3*32*32)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+def getnet(**kwargs):
+    return GateNet()
+
+
+if __name__ == '__main__':
+    net = GateNet()
+    input = torch.autograd.Variable(torch.randn(784))
+    y = net(input)
+    print(net)
+    print(y.size())
