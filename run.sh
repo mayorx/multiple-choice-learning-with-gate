58fceb6a1d50998da874906e532e5cf126ddfe4c
mayorx <chenliangyux@gmail.com>
2018-04-21 00:45:28 +0800

    validate - add 'verbose' to print verbose results.

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 # CUDA_VISIBLE_DEVICES=0 python main.py --epoch 300 --batch-size 64 -ct 100
 
 # for resnet
-CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 10 --model-num 2 --gate-type 3 --name mcl-twostep-3 --resume result/dcl-cifar-10-mcl-twostep-1/checkpoint.pth --evaluate
+#CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 10 --model-num 2 --gate-type 3 --name mcl-twostep-3 --resume result/dcl-cifar-10-mcl-twostep-1/checkpoint.pth --evaluate
+
+CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 2 --gate-type 3 --name unknown --evaluate --resume result/dcl-cifar-100-mcl-twostep-2/checkpoint.pth
