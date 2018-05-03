b5ff5a0145696e30aac488dc632b89c6ce592e7c
mayorx <chenliangyux@gmail.com>
2018-05-04 01:17:05 +0800

    mcl gateonly for cifar 10

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name unknwon
 
-CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 64 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-gateonly-regular --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-4/checkpoint.pth
+#CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 64 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-gateonly-regular --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-4/checkpoint.pth
+
+CUDA_VISIBLE_DEVICES=1 python main.py --epoch 200 --batch-size 64 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 10 --model-num 5 --gate-type 3 --name mcl-gateonly-regular --resume result/dcl-cifar-10-mcl-for-cmcl/checkpoint.pth
+
