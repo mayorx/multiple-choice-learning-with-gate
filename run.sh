fab2d56dac55aefe4510e512cf21f31b1e0fd91e
mayorx <chenliangyux@gmail.com>
2018-05-12 06:30:48 +0800

    ie, bs

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # CUDA_VISIBLE_DEVICES=0 python main.py --epoch 300 --batch-size 64 -ct 100
 
 # for resnet
-CUDA_VISIBLE_DEVICES=1 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name ie2
+CUDA_VISIBLE_DEVICES=1 python main.py --epoch 200 --batch-size 256 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name ie3
 
 
