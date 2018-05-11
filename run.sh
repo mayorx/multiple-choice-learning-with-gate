fb538081c7277e38c934cce3b00f95e9465c849d
mayorx <chenliangyux@gmail.com>
2018-05-12 02:55:18 +0800

    hyper parameter

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 64 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name unknwon
 
-CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-gateonly-regular-entropy12 --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-12/checkpoint.pth
+CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-gateonly-regular-entropy15 --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-15/checkpoint.pth
