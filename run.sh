d3251c6c4cd2614577c077e374614ac45b754e0e
mayorx <chenliangyux@gmail.com>
2018-05-14 05:33:56 +0800

    kl dis

--- run.sh
+++ run.sh
@@ -9,7 +9,16 @@
 #CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-gateonly-regular-entropy15 --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-15/checkpoint.pth
 #CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 5 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name mcl-gateonly-gateentropy --resume result/dcl-cifar-100-mn-5-gt-3-mcl-twostep-penalty-neg-overconfident-bugfix-penaltynum-3/checkpoint-epoch-0.pth
 
-CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name ie-kl --resume result/dcl-cifar-100-mn-5-gt-3-mcl-twostep-4/checkpoint.pth --evaluate
+#mcl + overlap3
+#CUDA_VISIBLE_DEVICES=0 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name mcl-overlap3-2 --resume result/dcl-cifar-100-mn-5-gt-3-mcl-twostep-4/checkpoint.pth
+
+#ie
+#CUDA_VISIBLE_DEVICES=1 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name ie-kl --resume result/dcl-cifar-100-mn-5-gt-3-ie3/checkpoint.pth
+
+#cmcl + overlap3
+#CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 128 --lr 0.03 --momentum 0.9 --wd 5e-4 -ct 100 --model-num 5 --gate-type 3 --name cmcl-overlap3-1 --resume result/dcl-cifar-100-mn-5-gt-3-mcl-entropy-17/checkpoint.pth
+
+
 
 
 
