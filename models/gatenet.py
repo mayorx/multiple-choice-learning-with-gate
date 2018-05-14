6160909361030671d248a6b76225150edefc9911
mayorx <chenliangyux@gmail.com>
2018-05-15 05:13:22 +0800

    gatenet fc2

--- models/gatenet.py
+++ models/gatenet.py
@@ -6,12 +6,14 @@ class GateNet(nn.Module):
     def __init__(self, model_nums):
         super(GateNet, self).__init__()
         self.fc1 = nn.Linear(64, 500)
-        self.fc2 = nn.Linear(500, model_nums)
+        self.fc2 = nn.Linear(500, 100)
+        self.fc3 = nn.Linear(100, model_nums)
 
     def forward(self, x):
         # x = x.view(-1, 3*32*32)
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.fc3(x)
         # x = self.fc2(x)
         # x = self.fc2(x)
         return x
