2aec946642aae660d70beb494cbaaab0ef522cae
mayorx <chenliangyux@gmail.com>
2018-05-16 16:33:39 +0800

    model!

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # CUDA_VISIBLE_DEVICES=0 python main.py --epoch 300 --batch-size 64 -ct 100
 
 # for resnet
-CUDA_VISIBLE_DEVICES=1 python main.py --epoch 200 --batch-size 128 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 10 --model-num 5 --gate-type 10
+CUDA_VISIBLE_DEVICES=2 python main.py --epoch 200 --batch-size 128 --lr 0.1 --momentum 0.9 --wd 5e-4 -ct 10 --model-num 4 --gate-type 10
