4358cea50107efdcbb8a55c2bda21d655bb2db85
mayorx <chenliangyux@gmail.com>
2018-05-16 17:40:47 +0800

    no data augment

--- main.py
+++ main.py
@@ -123,8 +123,8 @@ def main():
             train=True, 
             download=True,
             transform=transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
+                # transforms.RandomCrop(32, padding=4),
+                # transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 normalize,
             ]))
